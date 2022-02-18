4b193bd01c65f15ddd90668ca1083b623e501a5f
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-02-19 00:24:23 +0100

    greatly improved title extraction #1xd7gbZ

--- irisaparser/title.py
+++ irisaparser/title.py
@@ -11,14 +11,8 @@ import time
 
 init() #initialize colorama 
 import logging
-import logging.config
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': True,
-})
+
 log = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
-log.info('imported title final')
 
 
 #################### INPUT / COMMON ####################
@@ -28,12 +22,15 @@ for each file in specified directory
 '''
 
 def parseDir(directory):
+    result = []
     for filename in os.listdir(directory):
         pdf_file_path = os.path.join(directory, filename)
-        log.info('now parsing ' + pdf_file_path)
+        print(Fore.MAGENTA + '[<]INPUT : PARSING "'+pdf_file_path+'"' + Fore.RESET)
+
         if os.path.isfile(pdf_file_path):
             with pdfplumber.open(pdf_file_path) as pdf:
                 title = parseTitle(pdf)
+                result.append(title)
 
 #################### FILTERS / CROP ####################
 
@@ -91,26 +88,19 @@ def filter_potential_titles(titles):
         # catch wrong titles
         if(valid==True):
             res.append(title)
+    log.debug("filtered matches :" + str(res))
+    return res
+
+
+def filter_matches(titles):
+    res = titles
+    for i in res:
+        i = i.strip()
+        i = ''.join(i.split())
+    res = list(dict.fromkeys(res))
     return res
 
 #################### PARSING ####################
-
-    
-def line_potential_line_matcher(lines,potential_title):
-    sentence = ''
-    res = []
-    for p_title in potential_title:
-        for line in lines:
-            scan = line.strip()
-            if(p_title.find(scan)):
-                sentence = sentence + line + ' '
-        res.append(sentence)
-        sentence = ''
-    print(Fore.CYAN + 'matches : ' + Fore.RESET,end='')
-    print(res)
-
-
-
 
 def get_sentences(pdf: pdfplumber.PDF,max_sentences,font_list):
     """ return array with x largest sentences in pdf file """
@@ -122,7 +112,6 @@ def get_sentences(pdf: pdfplumber.PDF,max_sentences,font_list):
     potent_title = ''
     while i < num_sentences:
         for char in page.chars:
-            #print('i:' + str(i) + ' char: ' + char.get('text'))         
             if(char.get('size') == font_list[i]):
                 potent_title += char.get('text')
         res.append(potent_title)
@@ -160,6 +149,37 @@ def extract_potential_titles(page,largestFonts):
                 scan = scan + char.get('text')
         sentences.append(scan)
     return sentences
+
+
+def parse_potential_titles(lines,potential_titles):
+    res = []
+    for pot_title in potential_titles:
+        prev = ''
+        for line in lines:
+            raw_title = pot_title.replace(" ", "")
+            raw_line = line.replace(" ", "")
+
+                        
+            #print(Fore.BLUE + 'raw : "' + Fore.RESET + raw_title + Fore.BLUE + '"\n' + Fore.RESET, end=' ')
+            #print(Fore.CYAN + 'line : "' + Fore.RESET + raw_line + Fore.CYAN + '"\n' + Fore.RESET)
+            if(raw_line==raw_title):
+                res.append(line)
+            # try with previous iteration
+            prev_raw = prev.replace(" ", "")
+            concat_prev = prev_raw + raw_line
+            if(concat_prev == raw_title):
+                res.append((prev + " " + line))
+            prev=line
+                
+    return res
+
+def get_title_metadata(pdf:pdfplumber.PDF):
+    meta_title=pdf.metadata.get('Title')
+    if(meta_title is None or (len(meta_title.strip())==0)):
+        print(Fore.RED + "[*]ERROR : NO VALID METADATA FOUND" + Fore.RESET)
+        return None
+    else:
+        return meta_title
     
 def parseTitle(pdf:pdfplumber.PDF):
     # get Only 1/3 of pdf's first page
@@ -179,12 +199,34 @@ def parseTitle(pdf:pdfplumber.PDF):
     # only use fonts above threshold
     largestFonts = filterFonts(largestFonts)
 
-    # find in whole page, sequences containing largest fonts
+    # find in whole page, sequences of text matching largest fonts
     potential_titles = extract_potential_titles(page,largestFonts)    
     
+    # filtering ridiculously long or short files and containing some characters
     potential_titles = filter_potential_titles(potential_titles)
-    print(potential_titles)
-    final_title=''
+    
+    matched = parse_potential_titles(lines,potential_titles)
+    
+    # filter duplicates
+    matched = filter_matches(matched)
+    final_title = ''
+    errcount = 0
+    if(len(matched)>1):
+        errcount = errcount +1
+        print(Fore.BLUE + "[*]WARN : multiple titles ! selecting [0] by default" + Fore.RESET)
+        final_title = matched[0]
+    
+    if(len(matched)==0):
+        print(Fore.BLUE + "[*]WARN : NO REGULAR TITLE FOUND ! falling back on metadata" + Fore.RESET)
+        meta_title = get_title_metadata(pdf)
+        if(meta_title is None):
+            print(Fore.BLUE + "[*]WARN : NO TITLE METADATA FOUND ! falling back on first line..." + Fore.RESET)
+            final_title = lines[0]
+
+    if(len(matched)==1):
+        final_title = matched[0]
+    
+    print(Fore.GREEN + '[>]RESULT : "' + Fore.RESET + final_title + Fore.GREEN + '"\n')
     return final_title
     
     
