9ffebb45c31cb52950d48432efbe2f90db78ffdb
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-02-25 13:13:33 +0100

    corrected names for snake case

--- irisaparser/title.py
+++ irisaparser/title.py
@@ -27,7 +27,7 @@ def parseDir(directory):
 
         if os.path.isfile(pdf_file_path):
             with pdfplumber.open(pdf_file_path) as pdf:
-                title = parseTitle(pdf)
+                title = parse_title(pdf)
                 result.append(title)
 
 #################### FILTERS / CROP ####################
@@ -73,6 +73,7 @@ def filterFonts(fonts):
     
 
 def filter_potential_titles(titles):
+    '''Filter invalid titles (too short,too long)'''
     res = []
     
     for title in titles:
@@ -120,11 +121,9 @@ def get_sentences(pdf: pdfplumber.PDF,max_sentences,font_list):
 
     return res  
 
-def getPageLargestFontsList(page,fontAmount):
+def get_page_largest_fonts_list(page,font_amount):
     """ Return array with x largest lines in page """
-    res = {}
-    font_list_uniq = []
-    current_font=0.0
+
     page_fonts = []
     # find every fonts in page
     for char in page.chars:
@@ -135,14 +134,14 @@ def getPageLargestFontsList(page,fontAmount):
     
     # sort fonts from largest to smallest
     page_fonts.sort(reverse=True)
-    page_fonts = page_fonts[:fontAmount-1]
+    page_fonts = page_fonts[:font_amount-1]
     
     return page_fonts
 
 
-def extract_potential_titles(page,largestFonts):
+def extract_potential_titles(page,largest_fonts):
     sentences = []
-    for font in largestFonts:
+    for font in largest_fonts:
         scan = ''
         for char in page.chars:
             if(round(char.get('size'),4) == font):
@@ -160,9 +159,6 @@ def parse_potential_titles(lines_input,potential_titles):
             raw_title = pot_title.replace(" ", "")
             raw_line = line.replace(" ", "")
 
-                        
-            #print(Fore.BLUE + 'raw : "' + Fore.RESET + raw_title + Fore.BLUE + '"\n' + Fore.RESET, end=' ')
-            #print(Fore.CYAN + 'line : "' + Fore.RESET + raw_line + Fore.CYAN + '"\n' + Fore.RESET)
             if(raw_line==raw_title):
                 res.append(line)
             # try with previous iteration
@@ -171,7 +167,6 @@ def parse_potential_titles(lines_input,potential_titles):
             if(concat_prev == raw_title):
                 res.append((prev + " " + line))
             prev=line
-    print(res)
     return res
 
 def filter_bad_metadata(meta):
@@ -200,7 +195,7 @@ def get_title_metadata(pdf:pdfplumber.PDF):
     
     return meta_title
     
-def parseTitle(pdf:pdfplumber.PDF):
+def parse_title(pdf:pdfplumber.PDF):
     # get Only 1/3 of pdf's first page
     page = crop_first_page(pdf)
     
@@ -213,13 +208,13 @@ def parseTitle(pdf:pdfplumber.PDF):
     lines = filter_lines(lines)
     
     # fetch only 5 largest fonts in page
-    largestFonts = getPageLargestFontsList(pdf,5)
+    largest_fonts = get_page_largest_fonts_list(pdf,5)
     
     # only use fonts above threshold
-    largestFonts = filterFonts(largestFonts)
+    largest_fonts = filterFonts(largest_fonts)
 
     # find in whole page, sequences of text matching largest fonts
-    potential_titles = extract_potential_titles(page,largestFonts)    
+    potential_titles = extract_potential_titles(page,largest_fonts)    
     
     # filtering ridiculously long or short files and containing some characters
     potential_titles = filter_potential_titles(potential_titles)
