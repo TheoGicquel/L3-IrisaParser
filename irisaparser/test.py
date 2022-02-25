2ea81452f98ef1ec77983b6372088b321c9eb936
Corentin <corentin-quiniou@outlook.com>
2022-02-25 12:43:38 +0100

    maj tika

--- irisaparser/test.py
+++ irisaparser/test.py
@@ -1,10 +1,8 @@
 import pdfplumber as pp
 import sys
 import os
+from tika import parser 
 
-#sys.stdout.encoding = 'utf-8'
-
-characters = "I"
 
 corpusPath = "../Corpus_2021"
 
@@ -12,25 +10,30 @@ output_file = open("output.txt", "wb")
 
 file = os.path.join(corpusPath,"Nasr.pdf")
 
-with pp.open(file) as pdf:
-    first_page = pdf.pages[0]
+parsed = parser.from_file(file)  
 
-    text = first_page.extract_text(y_tolerance=0.5)
-    #text = text.replace(characters,"")
-# print(text + "\n")
-print(type(text))
-output_file.write(text.encode())
-output_file.close()
+#with pp.open(file) as pdf:
+#    first_page = pdf.pages[0]
+#
+#    text = first_page.extract_text(y_tolerance=0.5)
 
-test_file = open("test.txt", "wb")
 
-print(len(first_page.chars))
-print(first_page.chars[0].keys())
+#print(type(text))
+#output_file.write(text.encode())
+#output_file.close()
+#
+#test_file = open("test.txt", "wb")
+#
+#print(len(first_page.chars))
+#print(first_page.chars[0].keys())
 
-for index,obj in enumerate(first_page.chars):
-    test_line = str("> "+obj['text']+"("+str(obj['x0'])+","+str(obj['y0'])+")->("+str(obj['x1'])+","+str(obj['y1'])+")\n")
-    test_file.write(test_line.encode('utf-8'))
-    print(index)
-    if(index == 5): break
+#for index,obj in enumerate(first_page.chars):
+#    test_line = str("> "+obj['text']+"("+str(obj['x0'])+","+str(obj['y0'])+")->("+str(obj['x1'])+","+str(obj['y1'])+")\n")
+#    test_file.write(test_line.encode('utf-8'))
+#    print(index)
+#    if(index == 5): break
+
+file.write(parsed["content"].encode())
+
 
 test_file.close()
