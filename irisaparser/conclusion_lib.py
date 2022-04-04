871c189b7118693361ce48b2eb6af78c35b0082f
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-04-04 10:03:31 +0200

    added keywords for corpus

--- irisaparser/conclusion_lib.py
+++ irisaparser/conclusion_lib.py
@@ -4,8 +4,13 @@ if __name__ == "__main__":
     parsed = tp.from_file("./tests/corpus/single/Boudin-Torres-2006.pdf")
 # --------------------------------------------------------- #
 
+# 'Conclusions and Future Work' 'conclusion' 'conclusion and perspectives' 'conclusion and future work' 'Discussion' ' 
+
+keywords = ['CONCLUSION','RESULTS']
+
 
 def getConclusion(parsed):
+
     concl = parsed["content"].upper().find('CONCLUSION')
     return concl
 
