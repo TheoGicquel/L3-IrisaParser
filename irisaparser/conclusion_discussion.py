3cd37955722f2ceb5045c1f58039fe8bad1c1154
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-04-11 13:10:45 +0200

    draft for hybrid tika and plumber

--- irisaparser/conclusion_discussion.py
+++ irisaparser/conclusion_discussion.py
@@ -2,15 +2,30 @@
 
 if __name__ == "__main__":
     import pdfplumber
+    import tika.parser as tp
     parsed = tp.from_file("./tests/corpus/single/Boudin-Torres-2006.pdf")
+    
 # --------------------------------------------------------- #
 
 # 'Conclusions and Future Work' 'conclusion' 'conclusion and perspectives' 'conclusion and future work' 'Discussion' '  
 # after ACKNOWLEDGEMENT 'ACKNOWLEDGEMENTS' 'REFERENCES' 'APPENDIX *' '7 Follow-Up Work'
 keywords = ['CONCLUSION','RESULTS']
 
+    
+def getConclusionPosition(file):
+    content = file["content"]
+    keywords_conclusion = ['CONCLUSION','CONCLUSIONS','RESULTS','CONCLUSIONS AND FUTURE WORK','CONCLUSION AND PERSPECTIVES','CONCLUSIONS']
 
-def getConclusion(parsed):
+    conclBegPos = None
+    for l in keywords_conclusion:
+        conclBegPos = content.upper().find(l)
+        if(conclBegPos!=None and conclBegPos !='-1'):
+            print(conclBegPos)
+
+
+
+
+def getConclusion(filePath):
     content = parsed["content"]
     conclBegPos = content.upper().find('DISCUSSION AND FUTURE WORK')
     remainder = content[conclBegPos:]
@@ -20,5 +35,5 @@ def getConclusion(parsed):
     return remainder
 
 # ---------- DEV ONLY (remove before production )---------- #
-print(getConclusion(parsed))
+print(getConclusionPosition(parsed))
 # --------------------------------------------------------- #
