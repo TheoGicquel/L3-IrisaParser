929d71cac05a754ccf75aaf56c46ce5f7cccb127
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-04-22 12:24:44 +0200

    improved conclusion extraction

--- irisaparser/conclusion_discussion.py
+++ irisaparser/conclusion_discussion.py
@@ -1,7 +1,4 @@
-from turtle import pos
-from pyparsing import line
 from tika import parser as tp
-import re
 # ---------- TEST ONLY (remove before production )---------- #
 if __name__ == "__main__":
     filepath = "./tests/corpus_large/Torres.pdf"
@@ -10,31 +7,50 @@ if __name__ == "__main__":
     
 # 'Conclusions and Future Work' 'conclusion' 'conclusion and perspectives' 'conclusion and future work' 'Discussion' '  
 # after ACKNOWLEDGEMENT 'ACKNOWLEDGEMENTS' 'REFERENCES' 'APPENDIX *' 'Follow-Up'
-endKeywords = ['ACKNOWLEDGMENT','ACKNOWLEDGEMENTS','REFERENCES','APPENDIX','FOLLOW-UP']
-
+debug = False
+def dprint(input):
+    if(debug):
+        print(input)
 
 def getConclusion(parsed):
-    keywords = ['CONCLUSION','RESULT']
+    endKeywords = ['ACKNOWLEDGMENT','ACKNOWLEDGEMENTS','REFERENCES','APPENDIX','FOLLOW-UP']
+    beginKeywords = ['CONCLUSION','RESULT']
     content = parsed["content"]
-    res = []
+    resBegin = []
+    resEnd = []
     resultText = []
     lines = content.split('\n\n') # hopefully separate paragraphs
     
-    for k in keywords:
+    for k in beginKeywords:
         # manage priorities of keywords (i.e Conclusion > Result)
-        if(len(res)>0):
+        if(len(resBegin)>0):
             break
         for index,l in enumerate(lines):
             posConc = l[0:15].upper().find(k)
             if(posConc > -1):
-                print("found :'"+l+"' at pos:" + str(index) )
-                res.append(index+1) # we assume next paragraph is conclusion body
+                dprint("found :'"+l+"' at pos:" + str(index) )
+                resBegin.append(index+1) # we assume next paragraph is conclusion body
     
+    BeginArea = resBegin[-1] # use farthest match in document
+    for k in endKeywords:
+        if(len(resEnd)>0): # break loop at first 
+            break
+        for index,l in enumerate(lines):
+            posNextParagraph = l[0:15].upper().find(k)
+            if(posNextParagraph > -1):
+                dprint("found :'"+l+"' at pos:" + str(index) )
+                resEnd.append(index) # we assume next paragraph is conclusion body
+    EndArea = resEnd[-1]
+    dprint("--- endarea----")
+    dprint(lines[EndArea])
+    dprint("---------------")
     
-    lastMatch = res[-1] # use farthest match in document
-    print(lastMatch)
-    conclusionBody=lines[lastMatch]
-    print(conclusionBody)
+    dprint("result: begin at line [" + str(BeginArea) + '] ends at [' + str(EndArea) + "]")
+    endArray = lines[BeginArea:EndArea]
+    final = ''
+    for l in endArray:
+        final = f'{final}{l}'
+    return final
     
     
 
@@ -42,4 +58,5 @@ def getConclusion(parsed):
 # ---------- TEST ONLY (remove before production )---------- #
 if __name__ == "__main__":
     res = getConclusion(parsed)
+    print(res)
 # ---------------------------------------------------------- #
