42ee6dd3e346aa5ab9469877c21d1d0a4dacc238
Samuel Josse <samueljosse3553@gmail.com>
2022-05-13 12:15:20 +0000

    Update irisaparser/intro_lib.py

--- irisaparser/intro_lib.py
+++ irisaparser/intro_lib.py
@@ -1,26 +1,50 @@
-# from tika import parser  
-# import os
+from tika import parser  
+
+import os
 import re
 
+pdf = []
 
+parsed = parser.from_file("./PDF/On_the_Morality_of_Artificial_Intelligence.pdf")
+
+fichier = open("./TXTTest/On_the_Morality_of_Artificial_Intelligence.txt", "wb")
+
+listPdf = [x for x in os.listdir("./") if x.endswith(".pdf")]
 
 def intro_extractor(parsed):
 
+    
 
     data = parsed['content'] 
 
     intro = "1 I n t r o d u c t i o n" #jing
     intro2 = "1 Introduction" #iria
+    intro3 = "1. Introduction" #survey 
+    intro4 = "1 Artificial Intelligence Leaves the Research Lab"
+
 
     if intro2 in data:
         tmp = intro2
         intro2 = intro
         intro = tmp
+
+    elif intro3 in data:
+        tmp = intro3
+        intro3 = intro
+        intro = tmp    
+    elif intro4 in data:
+        data = " "
+        return data
     elif not intro in  data:
         intro = re.search(r"\n(1|I).+(Introduction|INTRODUCTION)\n", data, re.I).group()
+    
 
+    
 
-    end = re.search(r"\n(2|II).[^0]+([a-zA-Z]+(\s[a-zA-Z]+).).\n", data, re.I).group()
+    end = "2. Description of some terminology extraction systems " #survey
+
+    if not end in data:
+        end = re.search(r"\n(2|II).[^0]+([a-zA-Z]+(\s[a-zA-Z]+).).\n", data, re.I).group()
 
         
     integer = data.find(intro)
@@ -41,8 +65,6 @@ def intro_extractor(parsed):
                 finalstr += "\n" + str(x)
             else :
                 finalstr += "\n\n" + str(x)
-
- 
     return finalstr[2:]
 
 
